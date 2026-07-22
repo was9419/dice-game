feb1d0fa75ab041519535c0b5cd85d9b616c99cb
was9419 <47761851+was9419@users.noreply.github.com>
2026-07-21 21:42:48 -0600

    Add files via upload

---
+++ index.js
@@ -0,0 +1,38 @@
+const allDice = document.querySelectorAll('img');
+
+allDice.forEach((img) => {
+  img.classList.add('myDice');
+});
+document.addEventListener('DOMContentLoaded', () => {
+  const images = [
+    '/Dicee+Challenge+-+Starting+Files/images/dice1.png',
+    '/Dicee+Challenge+-+Starting+Files/images/dice2.png',
+    '/Dicee+Challenge+-+Starting+Files/images/dice3.png',
+    '/Dicee+Challenge+-+Starting+Files/images/dice4.png',
+    '/Dicee+Challenge+-+Starting+Files/images/dice5.png',
+    '/Dicee+Challenge+-+Starting+Files/images/dice6.png',
+  ];
+
+  const dado1 = document.querySelector('.img1');
+  const dado2 = document.querySelector('.img2');
+  const result = document.querySelector('h1');
+
+  dado1.id = 'dado1';
+  dado2.id = 'dado2';
+  result.id = 'result';
+
+  const resultDado1 = Math.floor(Math.random() * images.length);
+  const resultDado2 = Math.floor(Math.random() * images.length);  
+
+dado1.src = images[resultDado1];
+dado2.src = images[resultDado2];
+
+if (resultDado1 > resultDado2) {
+    result.textContent = 'Player 1 Wins!';
+  } else if (resultDado2 > resultDado1) {
+    result.textContent = 'Player 2 Wins!';
+  } else {
+    result.textContent = 'Draw!';
+  }
+});
+
